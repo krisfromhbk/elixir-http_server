e1c3c9df4f6337c9cb8d25da407eb3f6c226670b
krisfromhbk <kreisnews@gmail.com>
2018-07-05 19:26:23 +1000

    Simple hello-world server

--- lib/http_server.ex
+++ lib/http_server.ex
@@ -12,7 +12,15 @@ defmodule HttpServer do
       :world
 
   """
-  def hello do
-    :world
+  def init(default_options) do
+    IO.puts "initializing plug"
+    default_options
+  end
+
+  def call(conn, options) do
+    IO.puts "calling plug"
+
+    conn
+    |> Plug.Conn.send_resp(200, "hello world")
   end
 end
